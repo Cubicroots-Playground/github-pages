e1c58097f6d5dca2db8bf6af0513b87308335817
cubicroot <github@cubicroot.xyz>
2024-10-20 10:22:13 +0200

    wip: try uploading single html file

---
+++ cmd/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"os"
+)
+
+// @title Matrix Reminder and Calendar Bot (RemindMe)
+// @version 2.0.0
+// @description API documentation for the matrix reminder and calendar bot. [Inprint & Privacy Policy](https://cubicroot.xyz/impressum)
+
+// @contact.name Support
+// @contact.url https://github.com/CubicrootXYZ/matrix-reminder-and-calendar-bot
+
+// @host your-bot-domain.tld
+// @BasePath /
+// @query.collection.format multi
+
+// @securityDefinitions.apikey APIKeyAuthentication
+// @in header
+// @name Authorization
+func main() {
+	os.Exit(0)
+}
